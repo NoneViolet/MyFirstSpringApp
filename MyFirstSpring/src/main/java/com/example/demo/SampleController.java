66b5f7debb52b290d6aeb1cd87707d8482aadbbb
hashimoto <lanshimaoli810@gmail.com>
2024-05-28 15:02:39 +0900

    私は綾鷹よりジャスミン茶派

--- MyFirstSpring/src/main/java/com/example/demo/SampleController.java
+++ MyFirstSpring/src/main/java/com/example/demo/SampleController.java
@@ -9,5 +9,7 @@ public class SampleController {
 	public String sample() {
 		return "初めてのSpring\n豊かなうまみとかろやかな後味\n綾鷹";
 	}
-
+	public String sample1() {
+		return "初めてのSpring\n豊かなうまみとかろやかな後味\n綾鷹2";
+	}
 }
