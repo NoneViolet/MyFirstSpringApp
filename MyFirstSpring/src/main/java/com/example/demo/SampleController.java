0db6f12291c1b22b812de0c7ed3ad5523e53dfa8
noa <snoachi@yahoo.co.jp>
2024-05-28 15:13:05 +0900

    3回目のコミット

--- MyFirstSpring/src/main/java/com/example/demo/SampleController.java
+++ MyFirstSpring/src/main/java/com/example/demo/SampleController.java
@@ -9,7 +9,14 @@ public class SampleController {
 	public String sample() {
 		return "初めてのSpring\n豊かなうまみとかろやかな後味\n綾鷹";
 	}
+
+	@GetMapping("/get2")
 	public String sample1() {
 		return "初めてのSpring\n豊かなうまみとかろやかな後味\n綾鷹2";
+	}
+
+	@GetMapping("/hw")
+	public String hw() {
+		return "Hello World!!";
 	}
 }
