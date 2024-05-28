0da86b27bb3c667e5363d91969f2d39a179b83bb
honma <kazuyuki_3735@yahoo.co.jp>
2024-05-28 14:54:26 +0900

    更新しました(1回目)

--- MyFirstSpring/src/main/java/com/example/demo/SampleController.java
+++ MyFirstSpring/src/main/java/com/example/demo/SampleController.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 	@GetMapping("/get")
 	public String sample() {
-		return "初めてのSpring";
+		return "初めてのSpring\n豊かなうまみとかろやかな後味\n綾鷹";
 	}
 
 }
